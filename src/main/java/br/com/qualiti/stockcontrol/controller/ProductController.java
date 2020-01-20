420a3e522db8676c2c3faa0d52f9bf543d154d13
pauloepifani <paulorepifani@gmail.com>
2020-01-20 02:26:19 -0300

    Create repository, service and controller for Product class

---
+++ src/main/java/br/com/qualiti/stockcontrol/controller/ProductController.java
@@ -0,0 +1,59 @@
+package br.com.qualiti.stockcontrol.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.qualiti.stockcontrol.model.Product;
+import br.com.qualiti.stockcontrol.service.ProductService;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+	
+	private ProductService productService;
+	
+	ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	@PostMapping
+	public Product create(@RequestBody Product product) {
+		return productService.create(product);
+	}
+	
+	@GetMapping
+	public List<Product> getAll() {
+		return productService.getAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Product getById(@PathVariable Long id) {
+		return productService.getById(id);
+	}
+	
+	@GetMapping("/name/{name}")
+	public List<Product> getByName(@PathVariable String name) {
+		return productService.getByName(name);
+	}
+	
+	@PutMapping("/{id}")
+	public Product update(@PathVariable Long id, Product product) {
+		return productService.update(id, product);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity delete(@PathVariable Long id) {
+		productService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
